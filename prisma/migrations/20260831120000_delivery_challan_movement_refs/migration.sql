bcc37e7451ee5117b0ea001d44654dd6869a517e
J K Reddy <jkreddy1210@gmail.com>
2026-08-31 17:06:18 +0530

    Use Prisma enum types for movement reference migration

--- prisma/migrations/20260831120000_delivery_challan_movement_refs/migration.sql
+++ prisma/migrations/20260831120000_delivery_challan_movement_refs/migration.sql
@@ -1,5 +1,8 @@
-ALTER TABLE "delivery_challans" ADD COLUMN "movementReason" TEXT NOT NULL DEFAULT 'ORIGINAL';
+CREATE TYPE "MovementReason" AS ENUM ('ORIGINAL', 'RETURN', 'REPLACEMENT', 'OTHER');
+CREATE TYPE "NoteType" AS ENUM ('CREDIT', 'DEBIT');
+
+ALTER TABLE "delivery_challans" ADD COLUMN "movementReason" "MovementReason" NOT NULL DEFAULT 'ORIGINAL';
 ALTER TABLE "delivery_challans" ADD COLUMN "againstVoucherNo" TEXT;
 ALTER TABLE "delivery_challans" ADD COLUMN "invoiceNo" TEXT;
-ALTER TABLE "delivery_challans" ADD COLUMN "noteType" TEXT;
+ALTER TABLE "delivery_challans" ADD COLUMN "noteType" "NoteType";
 ALTER TABLE "delivery_challans" ADD COLUMN "noteNo" TEXT;
